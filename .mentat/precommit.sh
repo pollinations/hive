36f0e241053f7f5c8a0b0f22816e123f489c5f44
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-01-14 01:59:12 +0000

    Generate Mentat Scripts

--- .mentat/precommit.sh
+++ .mentat/precommit.sh
@@ -1,8 +1,2 @@
-# Run checks for each app with package.json
-for APP in */; do
-    if [ -f "${APP}package.json" ]; then
-        cd "$APP"
-        npm run test -- --watchAll=false --ci --passWithNoTests
-        cd ..
-    fi
-done
+npm run lint -- --fix
+npm run build
--- .mentat/setup.sh
+++ .mentat/setup.sh
@@ -1,8 +1,2 @@
-# Install dependencies for each app with package.json
-for APP in */; do
-    if [ -f "${APP}package.json" ]; then
-        cd "$APP"
-        npm install --no-audit
-        cd ..
-    fi
-done
+npm install
+npm run build
