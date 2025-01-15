9bcaf919aaa9ba6d98b199e08d9cdce32392ac64
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-01-15 03:29:22 +0000

    Fix Mentat Scripts: npm run lint
    Error output:
    =====5 Lines Omitted=====
    
    ==============================
    Full log:[here ❌](https://mentat.ai/log/25a16879-854e-4996-89ce-a78820e856df)

--- .mentat/precommit.sh
+++ .mentat/precommit.sh
@@ -1,2 +1,4 @@
-npm run lint
+cd tarot-reader && npm run lint && npm run build && cd ..
+cd pollinations-image-show && npm run lint && npm run build && cd ..
+cd placeholder-generator && npm run lint && npm run build && cd ..
 npm test
