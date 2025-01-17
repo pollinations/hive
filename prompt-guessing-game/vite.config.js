775d517283bfd001ece3abf219ee27562464df4b
voodoohop <t.haferlach@gmail.com>
2025-01-17 01:42:10 +0100

    Update vite.config.js

--- prompt-guessing-game/vite.config.js
+++ prompt-guessing-game/vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/prompt-guessing-game/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
   },
   server: {
     port: 5173,
